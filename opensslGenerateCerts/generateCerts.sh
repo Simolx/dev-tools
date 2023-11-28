5f961511ddbf905ab91c9d8a0bb67b0e8a9bd64a
Simolx <simosfck@gmail.com>
2023-11-29 00:09:09 +0800

    support to generate server key for pkcs8

--- opensslGenerateCerts/generateCerts.sh
+++ opensslGenerateCerts/generateCerts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 function usage {
     echo ""
@@ -17,6 +18,8 @@ function usage {
     echo "  --serverpass serverKeyPassword       server key password"
     echo "  --ip serverCertIP                    server cert ip, default is empty"
     echo "  --host serverCertDomain              server cert domain, default is empty"
+    echo "  --pkcs8                              generate server key for pkcs8"
+    echo "  -h|--help                            show usage message"
     exit 0
 }
 
@@ -46,11 +49,16 @@ CLIENTPASS=""
 CLIENTPASSIN=""
 SERVERPASS=""
 SERVERPASSIN=""
+PKCS8SERVERKEY=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
       usage
+      ;;
+    --pkcs8)
+      PKCS8SERVERKEY="true"
+      shift
       ;;
     --cacert)
       [ -n "$2" ] && CAFILE="$2"
@@ -105,12 +113,12 @@ done
 
 if [ ! -f "${CAFILE}.key" ]; then
   showStepInfo "generate ca key and cert file: ${CAFILE}.key, ${CAFILE}.crt"
-  openssl req -x509 -newkey rsa:3072 -nodes -keyout "${CAFILE}.key" -out "${CAFILE}.crt" -days 3650 -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testCA"
+  openssl req -x509 -newkey rsa:3072 -nodes -keyout "${CAFILE}.key" -out "${CAFILE}.crt" -days 3650 -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testCA" -extensions v3_ca -addext "keyUsage=digitalSignature, nonRepudiation, keyEncipherment, keyCertSign"
   [ -n "$CAPASS" ] && encryptKey ${CAFILE}.key $CAPASS
   NEWFILES="\n${CAFILE}.key\n${CAFILE}.crt"
 elif [ ! -f "${CAFILE}.crt" ]; then
   showStepInfo "ca key file ${CAFILE}.key exists, generate ca cert file: ${CAFILE}.crt"
-  openssl req -x509 -new -key "${CAFILE}.key" -out "${CAFILE}.crt"  -days 3650 -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testCA" $CAPASSIN
+  openssl req -x509 -new -key "${CAFILE}.key" -out "${CAFILE}.crt"  -days 3650 -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testCA" -extensions v3_ca -addext "keyUsage=digitalSignature, nonRepudiation, keyEncipherment, keyCertSign" $CAPASSIN
   NEWFILES="\n${CAFILE}.crt"
 else
   showStepInfo "use exists ca key and cert file: ${CAFILE}.key, ${CAFILE}.crt"
@@ -119,16 +127,16 @@ fi
 if [ ! -f "${CLIENTFILE}.key" ]; then
   showStepInfo "generate client key and cert file: ${CLIENTFILE}.key, ${CLIENTFILE}.crt"
   openssl req -new -newkey rsa:3072 -nodes -keyout ${CLIENTFILE}.key -out ${CLIENTFILE}.csr -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testClient"
-  openssl x509 -req -in ${CLIENTFILE}.csr -CA ${CAFILE}.crt -CAkey ${CAFILE}.key -CAcreateserial -out ${CLIENTFILE}.crt -days 3650 $CAPASSIN
+  openssl x509 -req -in ${CLIENTFILE}.csr -CA ${CAFILE}.crt -CAkey ${CAFILE}.key -CAcreateserial -out ${CLIENTFILE}.crt -days 3650 -extfile <(printf "subjectAltName=IP:127.0.0.1") $CAPASSIN
   rm -f ${CLIENTFILE}.csr
   [ -n "$CLIENTPASS" ] && encryptKey ${CLIENTFILE}.key $CLIENTPASS
   NEWFILES="${NEWFILES}\n${CLIENTFILE}.key\n${CLIENTFILE}.crt"
 elif [ ! -f "${CLIENTFILE}.crt" ]; then
   showStepInfo "client key file ${CLIENTFILE}.key exists, generate client cert file: ${CLIENTFILE}.crt"
   openssl req -new -key ${CLIENTFILE}.key -out ${CLIENTFILE}.csr -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testClient" $CLIENTPASSIN
-  openssl x509 -req -in ${CLIENTFILE}.csr -CA ${CAFILE}.crt -CAkey ${CAFILE}.key -CAcreateserial -out ${CLIENTFILE}.crt -days 3650 $CAPASSIN
+  openssl x509 -req -in ${CLIENTFILE}.csr -CA ${CAFILE}.crt -CAkey ${CAFILE}.key -CAcreateserial -out ${CLIENTFILE}.crt -days 3650 -extfile <(printf "subjectAltName=IP:127.0.0.1") $CAPASSIN
   rm -f ${CLIENTFILE}.csr
-  NEWFILES="${NEWFILES}\n${CLIENTFILE}.key\n${CLIENTFILE}.crt"
+  NEWFILES="${NEWFILES}\n${CLIENTFILE}.crt"
 else
   showStepInfo "use exists client key and cert file: ${CLIENTFILE}.key, ${CLIENTFILE}.crt"
 fi
@@ -138,7 +146,13 @@ if [ ! -f "${SERVERFILE}.key" ]; then
   openssl req -new -newkey rsa:3072 -nodes -keyout ${SERVERFILE}.key -out ${SERVERFILE}.csr -subj "/C=CN/ST=Beijing/L=Haidian/O=devCompany/OU=testing/CN=testServer"
   openssl x509 -req -in ${SERVERFILE}.csr -CA ${CAFILE}.crt -CAkey ${CAFILE}.key -CAcreateserial -out ${SERVERFILE}.crt -days 3650 -extfile <(printf "subjectAltName=DNS:${DOMAIN},IP:${IP}") $CAPASSIN
   rm -f ${SERVERFILE}.csr
-  [ -n "$SERVERPASS" ] && encryptKey ${SERVERFILE}.key $SERVERPASS
+  if [ x"${PKCS8SERVERKEY}" == "xtrue" ]; then
+    [ -n "$SERVERPASS" ] && serverPassout="-passout pass:${SERVERPASS}" || serverPassout="-nocrypt"
+    openssl pkcs8 -in ${SERVERFILE}.key -topk8 -v1 PBE-SHA1-3DES -out encrypted_${SERVERFILE}.key ${serverPassout}
+    mv encrypted_${SERVERFILE}.key ${SERVERFILE}.key
+  else
+    [ -n "$SERVERPASS" ] && encryptKey ${SERVERFILE}.key $SERVERPASS
+  fi
   NEWFILES="${NEWFILES}\n${SERVERFILE}.key\n${SERVERFILE}.crt"
 elif [ ! -f "${SERVERFILE}.crt" ]; then
   showStepInfo "server key file ${SERVERFILE}.key exists, generate server cert file: ${SERVERFILE}.crt"
@@ -154,3 +168,5 @@ rm -f ${CAFILE}.srl
 
 showStepInfo "new files"
 echo -e ${NEWFILES}
+
+set +x
